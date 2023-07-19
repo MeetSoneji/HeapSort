6e15f8e2c1c254dcf375c4c479ad99dcb611e006
MeetSoneji <110390711+MeetSoneji@users.noreply.github.com>
2023-07-19 22:48:54 +0530

    Add files via upload

---
+++ HeapSort.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+void swap(int *a, int *b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+void heapify(int arr[], int n, int rootIndex)
+{
+    int largest = rootIndex;
+    int leftChild = 2 * rootIndex + 1;
+    int rightChild = 2 * rootIndex + 2;
+    if (leftChild < n && arr[leftChild] > arr[largest])
+    {
+        largest = leftChild;
+    }
+    if (rightChild < n && arr[rightChild] > arr[largest])
+    {
+        largest = rightChild;
+    }
+    if (largest != rootIndex)
+    {
+        swap(&arr[rootIndex], &arr[largest]);
+        heapify(arr, n, largest);
+    }
+}
+
+void heapSort(int arr[], int n)
+{
+    for (int i = n / 2 - 1; i >= 0; i--)
+    {
+        heapify(arr, n, i);
+    }
+    for (int i = n - 1; i >= 0; i--)
+    {
+        swap(&arr[0], &arr[i]);
+        heapify(arr, i, 0);
+    }
+}
+
+void printArray(int arr[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+int main()
+{
+    int arr[] = {64, 34, 25, 12, 22, 11, 90};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    printf("Original array: ");
+    printArray(arr, n);
+    heapSort(arr, n);
+    printf("Sorted array: ");
+    printArray(arr, n);
+    return 0;
+}
